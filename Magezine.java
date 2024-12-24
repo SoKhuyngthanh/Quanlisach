52b14b9082418145f2036a86638b03f69446490a
SoKhuyngthanh <nholanhathuy@gmail.com>
2024-12-25 05:32:05 +0700

    Update Magezine.java

--- Magezine.java
+++ Magezine.java
@@ -3,13 +3,15 @@ package quan_ly_tai_lieu;
 public class Magezine extends Document
 {
     private int id_release;
+    private String name_magezine;
     private int month_release;
     
     public Magezine() {}
-    public Magezine(String id, String name_nxb, int num_release, int id_release, int month_release)
+    public Magezine(String id, String name_nxb, int num_release, int id_release, String name_magezie, int month_release)
     {
         super(id, name_nxb, num_release);
         this.id_release = id_release;
+        this.name_magezine = name_magezine;
         this.month_release = month_release;
     }
     public void setId_release(int id_release)
@@ -28,6 +30,14 @@ public class Magezine extends Document
     {
         return this.month_release;
     }
+    public void setName_magezine(String name_magezine)
+    {
+        this.name_magezine = name_magezine;
+    }
+    public String getName_magezine()
+    {
+        return this.name_magezine;
+    }
 
     @Override
     public void display()
@@ -35,28 +45,8 @@ public class Magezine extends Document
         System.out.println("Magezine: - Id: " + getId());
         System.out.println("          - NXB: " + getName_nxb());
         System.out.println("          - So ban phat hanh: " + getRealease_num());
+        System.out.println("          - Ten tap chi " + getName_magezine());
         System.out.println("          - So phat hanh: " + getId_release());
         System.out.println("          - Thang phat hanh: " + getMonth_release());
     }
 } 
-
-/*package quan_ly_sach;
-
-import javax.swing.JTextArea;
-
-public class Magezine extends Document {
-    private int idRelease;
-    private int monthRelease;
-
-    public Magezine(String id, String nameNXB, int releaseNum, int idRelease, int monthRelease) {
-        super(id, nameNXB, releaseNum);
-        this.idRelease = idRelease;
-        this.monthRelease = monthRelease;
-    }
-
-    @Override
-    public void displayInfo(JTextArea textArea) {
-        textArea.append("Magezine ID: " + id + ", NXB: " + nameNXB + ", Release Number: " + releaseNum + ", ID Release: " + idRelease + ", Month Release: " + monthRelease);
-    }
-}
-*/
